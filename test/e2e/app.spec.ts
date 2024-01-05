eeaea7c7c941faecd0a1fbf6944a9dcc8d5bed1b
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-04 21:32:03 -0500

    🔨 Refactor folder testing e2e

--- test/e2e/app.spec.ts
+++ test/e2e/app.spec.ts
@@ -2,5 +2,4 @@ import { test, expect } from '@playwright/test'
 
 test('should navigate to the about page', async ({ page }) => {
   await expect(page.getByText('Welcome, John!')).toBeHidden()
-  console.log(page.url())
 })
