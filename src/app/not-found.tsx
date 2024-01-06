5ae471b17e3f0f9bc7cc22f9db481418e3c6b918
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-01-06 16:17:14 -0500

    Add styles page not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NotFoundPage() {
   return (
-    <div>
+    <div className='h-screen'>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href='/'>Return Home</Link>
