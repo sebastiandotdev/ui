cf89ae315a5ca4508284eeab0c948e333e759608
orisme12 <carlosbonet33@gmail.com>
2024-02-08 17:03:56 -0500

    🚀 Add changes ui component inputs

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,6 @@ export function Input(props: PropsInput) {
     <input
       {...props}
       className='w-full border text-sm rounded-md border-[#E4E7E9] outline-none focus:border-[#2563EB] py-2 px-3 pl-10 placeholder:text-xs md:placeholder:text-base'
-      required
     />
   )
 }
