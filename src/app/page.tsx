7c48690fe81090720887e54aa4b6e26d7f234453
Gerzon05 <gerzonarletrangel@gmail.com>
2024-02-17 11:22:56 -0500

    🚀 Added new section in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import OfertIphone from '@/components/ofert-iphone'
 import CaruselIphones from '@/components/carousel-iphones'
 import AppleWatch from '@/components/apple-watch'
 import WathcCarousel from '@/components/watch-carosel'
+import Image from 'next/image'
+import newletter from '../../public/newletter.png'
+import { Button, Input } from '@/components/ui'
 
 export default function Home() {
   return (
@@ -37,9 +40,9 @@ export default function Home() {
                   className='placeholder:text-gray-dark placeholder:font-bold  md:placeholder:text-[16px]  md:placeholder:text-center lg:placeholder:text-[18px] text-gray-dark focus:outline-none   focus:border-none  px-2 py-1 w-full'
                 />
               </div>
-              <button className='bg-[#1c4060] text-white py-3 font-light rounded-2xl hover:bg-opacity-75 hover:bg-black w-1/4 md:w-1/3 transition-all duration-1000 ease-in-out'>
+              <Button className='bg-[#1c4060] text-white py-3 font-light rounded-2xl hover:bg-opacity-75 hover:bg-black w-1/4 md:w-1/3 transition-all duration-1000 ease-in-out'>
                 Search
-              </button>
+              </Button>
             </div>
           </div>
         </section>
@@ -59,6 +62,33 @@ export default function Home() {
         <AppleWatch />
         <WathcCarousel />
         <div className='absolute inset-y-0 z-3 right-0 w-1/12 md:w-[11%] bg-gradient-to-l from-white to-transparent'></div>
+      </section>
+      <section className='p-10 md:p-20 bg-gray'>
+        <div className='bg-white rounded-lg md:flex p-5'>
+          <div className='w-full md:w-1/2 p-9 text-center md:text-left md:pl-10 lg:pl-20 flex flex-col gap-3 justify-center'>
+            <h2 className='font-bold text-xl sm:text-3xl md:text-2xl lg:text-3xl'>
+              Subscribe To Newsletter
+            </h2>
+            <p className='text-gray-dark font-semibold text-xs md:text-xs lg:text-sm '>
+              Get free guide about smart watches daily.{' '}
+            </p>
+            <div className='w-full  flex  md:flex-row items-center md:gap-6 justify-around shadow-md bg-gray py-2 px-1 rounded-lg'>
+              <div className='flex items-center gap-10 justify-between '>
+                <input
+                  type='text'
+                  placeholder='Enter Email Address'
+                  className='placeholder:text-gray-dark bg-gray placeholder:font-bold  md:placeholder:text-[12px] lg:placeholder:text-[14px] text-gray-dark focus:outline-none px-2  focus:border-none py-1 w-full'
+                />
+              </div>
+              <Button className='bg-purple text-white py-2 font-light rounded-lg hover:bg-opacity-75 w-1/4 md:w-1/3 transition-all duration-1000 ease-in-out md:text-xs lg:text-sm'>
+                Subscribe
+              </Button>
+            </div>
+          </div>
+          <div className='w-full md:w-1/2'>
+            <Image src={newletter} alt='imagen de newletter' />
+          </div>
+        </div>
       </section>
     </>
   )
